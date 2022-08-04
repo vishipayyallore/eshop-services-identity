df944ed0865b39f6e216462cded7345e58167a90
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-08-04 23:32:33 +0530

    Added Scope, and Client
    Also, retrieved the configuration using

---
+++ Source/IdentityServer.API/Configuration/Config.cs
@@ -0,0 +1,41 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer.API.Configuration;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope(name: "apitest", displayName: "Test API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client-apitest",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("NoPassword@1".Sha256()) /* Need to get this from Configuration */
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "apitest" }
+            }
+        };
+
+
+}
