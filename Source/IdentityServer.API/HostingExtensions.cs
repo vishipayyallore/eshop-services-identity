685d2a99664017be03f6e670b09c7cd84192ee50
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-08-11 21:48:58 +0530

    Added Test Users

--- Source/IdentityServer.API/HostingExtensions.cs
+++ Source/IdentityServer.API/HostingExtensions.cs
@@ -17,7 +17,8 @@ internal static class HostingExtensions
             })
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients);
+            .AddInMemoryClients(Config.Clients)
+            .AddTestUsers(TestUsers.Users);
 
         return builder.Build();
     }
