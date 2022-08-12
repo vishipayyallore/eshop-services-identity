2c3d02b54271fbb685cca145c64074d4da919d64
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-08-12 22:04:49 +0530

    Verified the Logout Funcationlity

---
+++ Tests/UseIden.DemoWeb/Pages/Signout.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class SignoutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+
+    }
+
+}
