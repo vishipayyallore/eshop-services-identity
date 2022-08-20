d2450b64537e88b132b7b4d8758500569e782719
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-08-20 15:20:27 +0530

    Build and Run Shell Script

---
+++ Scripts/a1-build-and-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Description: To Build and Run the Identity Web API Locally
+# Author: Swamy
+# Date: 20-Aug-2022
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+dotnet run --project ./Source/Identity.API/Identity.API.csproj -c release 
---
+++ Scripts/b1-containerize-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Description: Script to dockerize the application and push it to the docker registry
+# Author: Swamy
+# Date: 20-Aug-2022
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+TAG=latest
+VERSION_TAG=$(git log -1 --pretty=format:%h)
+echo "version tag: $VERSION_TAG"
+
+REPOSITORY=$DOCKER_USERNAME/$DOCKER_IMAGE_NAME
+
+docker login
+
+docker build -f "./Source/Products.API/Dockerfile" -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
+
+docker push $REPOSITORY:$TAG
+docker push $REPOSITORY:$VERSION_TAG
